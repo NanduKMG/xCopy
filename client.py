f46704c9e6ba2d8faed470554f23ef8cfc3b7c85
NikJoj <nikhiljoji07@gmail.com>
2018-11-28 08:14:47 +0530

    adding keycomb input

--- client.py
+++ client.py
@@ -4,6 +4,55 @@ import os
 import sys
 from socket import *
 
+from pynput import keyboard
+
+# The key combination to check
+COMBINATIONS = [
+    {keyboard.Key.ctrl, keyboard.KeyCode(char='c')}
+]
+
+COMBINATION = [
+    {keyboard.Key.ctrl, keyboard.KeyCode(char='q')}
+]
+
+# The currently active modifiers
+current = set()
+
+def execute_send():
+    print ("Sending")
+    os.system("xclip -o > copy.txt")
+    f3=open("copy.txt","r")
+    text=(f3.readline()).rstrip('\n')
+
+    #print text 
+
+    send_cmd="echo "+text+" | nc "+host+" 4000"
+    #print send_cmd
+    os.system(send_cmd)
+
+
+def execute_receive():
+    print ("Receiving")
+
+
+def on_press(key):
+    if any([key in COMBO for COMBO in COMBINATIONS]):
+        current.add(key)
+        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
+            execute_send()
+
+    if any([key in COMBO for COMBO in COMBINATION]):
+        current.add(key)
+        if any(all(k in current for k in COMBO) for COMBO in COMBINATION):
+            execute_receive()
+
+def on_release(key):
+    if any([key in COMBO for COMBO in COMBINATIONS]):
+        current.remove(key)
+
+
+
+
 cmd="nmap -sP 192.168.43.* | grep -o 192.* | tr -d '()' > ip.txt"
 os.system(cmd)
 
@@ -66,14 +115,9 @@ for ip in ips:
 #UDPSock.close()
 #os._exit(0)
 
-os.system("xclip -o > copy.txt")
-f3=open("copy.txt","r")
-text=(f3.readline()).rstrip('\n')
 
-#print text
 
-send_cmd="echo "+text+" | nc "+host+" 4000"
-#print send_cmd
-os.system(send_cmd)
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
 
 
